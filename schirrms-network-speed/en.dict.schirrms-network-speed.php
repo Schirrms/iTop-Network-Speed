a62caac7e7c5c7140782aaf3f2af07e3b6096fb9
Schirrms <schirrms@schirrms.net>
2019-08-20 12:45:53 +0200

    Glitch 0.2.0.1

--- schirrms-network-speed/en.dict.schirrms-network-speed.php
+++ schirrms-network-speed/en.dict.schirrms-network-speed.php
@@ -14,10 +14,11 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:NetworkSpeed/Attribute:bitspeed' => 'Connection speed in bit/s',
 	'Class:NetworkSpeed/Attribute:bitspeed+' => 'Set in this field the network speed in bit/s. Yes, the number can (and usualy will) be huge.',
 	'Class:NetworkSpeed/Attribute:comment' => 'Complementary notes',
-	'Class:NetworkSpeed/Attribute:humanspeed_id' => 'Human readable Speed',
-	'Class:IPInterface/Attribute:IPInterfaces_list' => 'All interfaces at this speed',
+	'Class:NetworkSpeed/Attribute:humanspeed_id' => 'Human Readable Speed',
+	'Class:NetworkSpeed/Attribute:IPInterfaces_list' => 'All interfaces at this speed',
+	'Class:IPInterface/Attribute:human_speed' => 'Human Readable Speed',
 	'Class:IPInterface/Attribute:humanspeed_id+' => 'Select here the interface speed. Clic \'+\' if the needed speed is not present.',
-	'Class:PhysicalInterface/Attribute:humanspeed_id' => 'Human speed',
+	'Class:PhysicalInterface/Attribute:humanspeed_id' => 'Human Readable Speed',
 	'Class:PhysicalInterface/Attribute:humanspeed_id+' => 'Select here the interface speed. Clic \'+\' if the needed speed is not present.',
 ));
 ?>
--- schirrms-network-speed/fr.dict.schirrms-network-speed.php
+++ schirrms-network-speed/fr.dict.schirrms-network-speed.php
@@ -10,14 +10,15 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	// Dictionary entries go here
 	'Class:NetworkSpeed' => 'Vitesse Réseau',
 	'Class:NetworkSpeed/Attribute:humanspeed' => 'Vitesse réseau format humain',
-	'Class:NetworkSpeed/Attribute:humanspeed+' => 'Champ en lecture seule. Affiche la vitesse réseau dans un format "humain". Il est calculé à partir de la vitesse en bit/s',
-	'Class:NetworkSpeed/Attribute:bitspeed' => 'Bitesse de la connexion en bit/s',
+	'Class:NetworkSpeed/Attribute:humanspeed+' => 'Champ en lecture seule. Affiche la vitesse réseau dans un format \'humain\'. Il est calculé à partir de la vitesse en bit/s',
+	'Class:NetworkSpeed/Attribute:bitspeed' => 'Vitesse de la connexion en bit/s',
 	'Class:NetworkSpeed/Attribute:bitspeed+' => 'Saisissez dans ce champs la vitesse en bit/s. Oui, le chiffre peut être (et sera probablement) énorme.',
 	'Class:NetworkSpeed/Attribute:comment' => 'Notes complémentaires',
 	'Class:NetworkSpeed/Attribute:humanspeed_id' => 'Vitesse en format humain',
-	'Class:IPInterface/Attribute:IPInterfaces_list' => 'Toutes les interfaces à cette vitesse',
+	'Class:NetworkSpeed/Attribute:IPInterfaces_list' => 'Toutes les interfaces à cette vitesse',
+	'Class:IPInterface/Attribute:humanspeed_id' => 'Vitesse humaine',
 	'Class:IPInterface/Attribute:humanspeed_id+' => 'Choisissez la vitesse de l\'interface. Cliquez \'+\' si votre vitesse n\'est pas présente.',
-	'Class:PhysicalInterface/Attribute:IPInterfaces_list' => 'Toutes les interfaces à cette vitesse',
+	'Class:PhysicalInterface/Attribute:humanspeed_id' => 'Vitesse humaine',
 	'Class:PhysicalInterface/Attribute:humanspeed_id+' => 'Choisissez la vitesse de l\'interface. Cliquez \'+\' si votre vitesse n\'est pas présente.',
 ));
 ?>
